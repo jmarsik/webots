fb720197b67c406d077918573e8602c570f2c6f2
Yann LEROY <yann.leroy102@orange.fr>
2021-05-18 12:25:19 +0200

    fix comma

--- include/controller/c/webots/robot.h
+++ include/controller/c/webots/robot.h
@@ -66,7 +66,7 @@ int wb_robot_init();
  */
 #else
 int wb_robot_init_msvc();  // internally, this function just calls wb_robot_init()
-#define wb_robot_init() do{setvbuf(stdout, NULL, _IONBF, 0); setvbuf(stderr, NULL, _IONBF, 0); wb_robot_init_msvc()}while(0)
+#define wb_robot_init() do{setvbuf(stdout, NULL, _IONBF, 0); setvbuf(stderr, NULL, _IONBF, 0); wb_robot_init_msvc();}while(0)
 #endif
 
 int wb_robot_step(int duration);  // milliseconds
@@ -74,7 +74,7 @@ int wb_robot_step(int duration);  // milliseconds
 #ifdef __CYGWIN__  // In that case, we need to flush explicitly the stdout/stdin streams otherwise they are buffered
 // We cannot call fflush from the libController as libController is compiled with gcc8 and won't flush the stdout/stderr
 // of a gcc7 (cygwin) compiled binary. Therefore, we need to perform the fflush in a gcc7 compiled code, e.g., in a macro here.
-#define wb_robot_step(d) do{fflush(NULL); wb_robot_step(d)}while(0)
+#define wb_robot_step(d) do{fflush(NULL); wb_robot_step(d);}while(0)
 #endif
 
 WbUserInputEvent wb_robot_wait_for_user_input_event(WbUserInputEvent event_type, int timeout);  // milliseconds
